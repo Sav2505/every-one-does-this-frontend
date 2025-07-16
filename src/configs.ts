760f004e360084c4497aee92f8d470a41a54f66e
Sav2505 <sasa25051995@gmail.com>
2025-07-16 22:18:49 +0300

    Initial frontend commit

---
+++ src/configs.ts
@@ -0,0 +1,10 @@
+export enum CONFIG_API {
+  SERVER_PORT = 5000,
+  SOCKET_PORT = 5001,
+  // BASE_URL= "http://localhost",
+  BASE_URL = 'https://ehlafdwgnadqtdpxxiwc.supabase.co/rest/v1',
+  TABLES_ROUTER = "tables",
+  MAIN_PAGE = "/",
+  CONTROL_PAGE = "/control",
+  MOBILE_PAGE = "/mobile"
+};
